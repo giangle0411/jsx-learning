c2e1893db8b3c263d7a45b6b72bee92d3146426e
Giang-Le <giangle0411@gmail.com>
2020-06-08 21:26:35 +1000

    class component, state

--- src/index.js
+++ src/index.js
@@ -1,60 +1,25 @@
 //Import the React and ReactDOM libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CommentDetail from './CommentDetails'
-import ApprovalCard from './ApprovalCard'
-import faker from 'faker'
 
 // Create a react component
-const App = () => {
-  const buttonText = { text: 'Click me!' }
-  const labelText = 'Enter name:'
 
-  return (
-    <div className="ui container comments">
-      <div className="nameInput">
-        <label className="label" htmlFor="name">
-          {labelText}
-        </label>
-        <input id="name" type="text" />
-        <button style={{ backgroundColor: 'blue', color: 'white' }}>
-          {buttonText.text}
-        </button>
-      </div>
-      <div className="ui container comments">
-        <ApprovalCard>
-          <div>
-            <h4>Warning!</h4>
-            Are you sure you want to continue?
-          </div>
-        </ApprovalCard>
-        <ApprovalCard>
-          <CommentDetail
-            name={faker.name.firstName()}
-            img={faker.image.avatar()}
-            timeAgo="Today at 4:30PM"
-            content="Nice blog post!"
-          />
-        </ApprovalCard>
-        <ApprovalCard>
-          <CommentDetail
-            name={faker.name.firstName()}
-            img={faker.image.avatar()}
-            timeAgo="Today at 8:20AM"
-            content="Useful information!"
-          />
-        </ApprovalCard>
-        <ApprovalCard>
-          <CommentDetail
-            name={faker.name.firstName()}
-            img={faker.image.avatar()}
-            timeAgo="Yesterday at 6:15PM"
-            content="Nice formatting."
-          />
-        </ApprovalCard>
-      </div>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { lat: null }
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude })
+      },
+      (err) => console.log(err)
+    )
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat} </div>
+  }
 }
 
 // Take the react component and show it on the screen
