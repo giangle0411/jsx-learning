7855fdc3b0de1fa1c53a023405feacd52cd4f7e3
Giang-Le <giangle0411@gmail.com>
2020-06-08 20:06:10 +1000

    Components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 //Import the React and ReactDOM libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetails'
+import ApprovalCard from './ApprovalCard'
+import faker from 'faker'
 
 // Create a react component
 const App = () => {
@@ -8,14 +11,48 @@ const App = () => {
   const labelText = 'Enter name:'
 
   return (
-    <div>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText.text}
-      </button>
+    <div className="ui container comments">
+      <div className="nameInput">
+        <label className="label" htmlFor="name">
+          {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>
+          {buttonText.text}
+        </button>
+      </div>
+      <div className="ui container comments">
+        <ApprovalCard>
+          <div>
+            <h4>Warning!</h4>
+            Are you sure you want to continue?
+          </div>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            name={faker.name.firstName()}
+            img={faker.image.avatar()}
+            timeAgo="Today at 4:30PM"
+            content="Nice blog post!"
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            name={faker.name.firstName()}
+            img={faker.image.avatar()}
+            timeAgo="Today at 8:20AM"
+            content="Useful information!"
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            name={faker.name.firstName()}
+            img={faker.image.avatar()}
+            timeAgo="Yesterday at 6:15PM"
+            content="Nice formatting."
+          />
+        </ApprovalCard>
+      </div>
     </div>
   )
 }
