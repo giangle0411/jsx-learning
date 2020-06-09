b5434169dea270e95f0989e1f02a0deb04e035b3
Giang-Le <giangle0411@gmail.com>
2020-06-09 18:57:58 +1000

    Life cycle, config class, spinner component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 //Import the React and ReactDOM libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 // Create a react component
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { lat: null, errorMessage: '' }
+  state = { lat: null, errorMessage: '' }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude })
@@ -19,14 +19,30 @@ class App extends React.Component {
     )
   }
 
-  render() {
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!')
+  }
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return (
+        <div>
+          <SeasonDisplay lat={this.state.lat} />
+        </div>
+      )
     }
-    return <div>Loading!</div>
+    return (
+      <div>
+        <Spinner message="Please allow location request!" />
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>
   }
 }
 
