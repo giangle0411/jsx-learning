0b4dccb98e2567d78ef424d8cca518a58b027187
Giang-Le <giangle0411@gmail.com>
2020-06-10 01:50:06 +1000

    New project - Search bar & state props

--- src/index.js
+++ src/index.js
@@ -1,50 +1,8 @@
 //Import the React and ReactDOM libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SeasonDisplay from './SeasonDisplay'
-import Spinner from './Spinner'
+import App from './components/App'
 // Create a react component
-
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' }
-
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
-    )
-  }
-
-  componentDidUpdate() {
-    console.log('My component was just updated - it rerendered!')
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div> Error: {this.state.errorMessage}</div>
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return (
-        <div>
-          <SeasonDisplay lat={this.state.lat} />
-        </div>
-      )
-    }
-    return (
-      <div>
-        <Spinner message="Please allow location request!" />
-      </div>
-    )
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>
-  }
-}
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'))
