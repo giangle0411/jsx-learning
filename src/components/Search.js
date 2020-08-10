0574944cc98f2ae5bdcfa856260f8172d4179f65
Giang-Le <giangle0411@gmail.com>
2020-08-10 23:58:29 +1000

    Search widget, useEffect

---
+++ src/components/Search.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+
+const Search = () => {
+  const [term, setTerm] = useState('programming')
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await Axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      })
+      setResults(data.query.search)
+    }
+    search()
+  }, [term])
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  })
+
+  return (
+    <div className="ui container">
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            className="input"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  )
+}
+
+export default Search
